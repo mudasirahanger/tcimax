2d73da837beed325c92f77dcdd94b9970c27aba8
mudasirahanger <mudasir.ahanger@digitalindia.gov.in>
2025-01-25 15:59:48 +0530

    csrf fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
+        $middleware->validateCsrfTokens(
+            except: ['*']
+        );
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
